7bb0e5b62e0c63bf1b971bcb601157c88fd291f4
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-12-07 15:04:30 +0530

    created script

--- Scripts/Chi-Square Test.R
+++ Scripts/Chi-Square Test.R
@@ -4,3 +4,24 @@ library(rstatix)
 library(car)
 library(here)
 library(easystats)
+library(janitor)
+
+
+# data
+
+dat <- iris %>% 
+    as_tibble() %>% 
+    mutate(size = case_when(Sepal.Length < median(Sepal.Length) ~ "Small",
+                            TRUE ~ "Big"))
+
+dat %>% 
+    tabyl(Species, size) %>% 
+    chisq.test()
+
+
+dat %>% 
+    tabyl(Species, size) %>% 
+    fisher.test()
+
+
+# End of script
